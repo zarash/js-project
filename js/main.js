66a34cac72dc33e831e2948f81b6348d4ccc3ea1
zarash <z.ashrafi@yahoo.com>
2019-05-01 00:05:58 +0430

    First commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function show (){
+     var income = document.getElementById("income").value;
+     var outcome = document.getElementById("outcome").value;
+     if (Number(income) > Number(outcome)){
+        document.getElementById("demo").innerHTML = 'shoma kamkharj hastid' ;
+     }else if (Number(income) == Number(outcome)){
+        document.getElementById("demo").innerHTML = 'mosavi' ;  
+     }else {
+        document.getElementById("demo").innerHTML = 'shoma pppooooor khard hastid' ; 
+     }
+     
+}
